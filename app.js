d5d2aa0ed8903fca3c0a9cba14550dbb80a682a3
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-09 13:52:36 +0530

    redirects and 404 page

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ res.redirect('/about');
 })
 
 /*404 page 
-.use is a middleware function 
-use function is going to fire for every request(req) coming in 
-when you type any ulr in the browser then it's going see is any match present in the express app if yes then shwo tha
-other wise it will  show the 404.html file 
+* use is a middleware function  
+* use function is going to fire for every request(req) coming from browser
+* when you type any ulr in the browser then it's going see is any match present in the express app if yes then shwo tha
+* other wise it will  show the 404.html file 
 */
 app.use((req,res) =>{
 res.sendFile("./views/404.html", { root: __dirname });
