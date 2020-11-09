44ac8effcf321d7929a2d399757c628e78724f2c
Irfan Ansari <irfanansari2k20@gmail.com>
2020-11-09 13:50:08 +0530

    request and 404 page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* Redirects and 404 */
+
 //import
 const express = require("express");
 
@@ -14,12 +16,25 @@ app.get("/about", (req, res) => {
 });
 
 
+//redirects
+//redirects method redirects the html page and also sets the status code
+
+app.get('/about-us',(req,res) =>{
+res.redirect('/about');
+})
+
+/*404 page 
+.use is a middleware function 
+use function is going to fire for every request(req) coming in 
+when you type any ulr in the browser then it's going see is any match present in the express app if yes then shwo tha
+other wise it will  show the 404.html file 
+*/
+app.use((req,res) =>{
+res.sendFile("./views/404.html", { root: __dirname });
+
+})
+
 //listen for request 
-app.listen(3000);
+app.listen((3000));
 
 
-/*Express 
-*You don't have to define status code manually
-*You don't have to define res content type manually
-*
- */
